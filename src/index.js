9a0fa6003100b1414eb1f6140831139a746c505b
tomekevolve <tomek@evolvedigital.co.uk>
2019-05-10 14:20:43 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Minimal React, Webpack & Babel setup!';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
