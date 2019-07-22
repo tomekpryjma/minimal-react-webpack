f62d8047ca9dd6a65dfb3956bc0563fd4eec9d77
tomekevolve <tomek@evolvedigital.co.uk>
2019-07-22 10:19:17 +0100

    Remove webpack.common and update webpack.prod to mirror common's ex-content

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,68 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = merge(common, {
-    mode: 'production',
-});
+module.exports = (env) => {
+    // Get the project root path (assumes the webpack config is at root level).
+    const currentPath = path.join(__dirname);
+
+    // Create the fallback path.
+    const basePath = currentPath + "/.env";
+
+    const envPath = basePath + "." + env.ENVIRONMENT;
+
+    // Check if the file exists, otherwise fall back to the production .env
+    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+    const fileEnv = dotenv.config({path: finalPath}).parsed;
+
+    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+        return prev;
+    }, {})
+
+    return {
+        entry: './src/index.js',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                /**
+                 * Used for importing images into React files.
+                 */
+                {
+                    test: /\.(png|jpe?g)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        }
+                    ]
+                },
+        
+                /**
+                 * Used for importing & compiling SASS.
+                 */
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+        output: {
+            path: __dirname + '/dist',
+            publicPath: '',
+            filename: 'bundle.js'
+        },
+        plugins: [
+            new webpack.DefinePlugin(envKeys)
+        ]
+    }
+};
